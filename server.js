a09567e042c631840bc6212c92c9a80fc84a3418
Guilherme Moura <guilhermermribeiro@gmail.com>
2023-03-14 21:39:50 -0300

    Instalei o bodyparse e fiz alguns teste do mesmo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const bodyParse = require('body-parser')
 
 app.listen(3000, function(){
     console.log("Conectou")
@@ -10,3 +11,9 @@ app.get('/', (req, res) => {
 })
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParse.urlencoded({extended: true}));
+
+app.post('/show', (req, res) => {
+    console.log(req.body)
+})
