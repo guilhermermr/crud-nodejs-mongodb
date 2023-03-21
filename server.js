774597b1a1c69097e3325e4d38edcdf28d81a688
Guilherme Moura <gumoura@uolinc.com.br>
2023-03-21 13:33:44 -0300

    Conexao com o Mongo

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const bodyParse = require('body-parser')
+const MongoClient = require('mongodb').MongoClient 
 
-app.listen(3000, function(){
-    console.log("Conectou")
+const uri = 'mongodb+srv://crud:crud123@cluster.jn4xhtl.mongodb.net/?retryWrites=true&w=majority'
+
+await = MongoClient.connect(uri, (err, client) => {
+    if(err) return console.log(err)
+    db = client.db('cluster')
+
+    app.listen(3000, function(){
+        console.log("Conectou")
+    })
 })
 
 app.get('/', (req, res) => {
@@ -14,6 +22,14 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParse.urlencoded({extended: true}));
 
+
 app.post('/show', (req, res) => {
-    console.log(req.body)
+    db.connect('data').save(req.body, (err, result) => {
+        if (err){
+            return console.log(err)
+        }
+
+        console.log('Salvo no banco de dados');
+        res.redirect('/')
+    })
 })
